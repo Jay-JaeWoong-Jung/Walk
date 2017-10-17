22b599d98be1a9c45477723b6d7e52edaa906c49
JayJung <jay.jaewoong.jung@gmail.com>
2017-10-17 10:54:06 +0900

    memberDAO MemberVO setup

--- src/model/MemberDAO.java
+++ src/model/MemberDAO.java
@@ -1,5 +1,11 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
 /*
  * 나중에 실질적으로 Component기반으로 최종적으로 돌릴때에는
  * DataSource 방식을 사용할 것이다...
@@ -19,4 +25,47 @@ package model;
  */
 public class MemberDAO {
 
+	private static MemberDAO dao = new MemberDAO();
+	private MemberDAO() {}
+	public static MemberDAO getInstance() {
+		return dao;
+	}
+	/////////////// 공통적인 로직 /////////////////////////////
+	public  Connection getConnection() throws SQLException{
+		System.out.println("디비연결 성공....");
+		return DataSourceManager.getInstance().getConnection();
+	}
+	public void closeAll(PreparedStatement ps, Connection conn)throws SQLException{
+		if(ps!=null) ps.close();
+		if(conn!=null) conn.close();
+	}
+	
+	public void closeAll(ResultSet rs,PreparedStatement ps, Connection conn)throws SQLException{
+		if(rs!=null){
+			rs.close();
+			closeAll(ps, conn);
+		}
+	}//
+	
+	
+	public static void main(String[] args) throws Exception{
+		
+		MemberDAO dao = MemberDAO.getInstance();
+		MemberVO vo = new MemberVO("myId", "myPass", 
+				"myUserName",
+				"01012345678",
+				new Date(2000, 11, 11 ),
+				0,
+				"my comapny",
+				0,
+				950411);
+		//dao.Businesslogic()
+		//System.out.println(dao.getPostingByNo());
+		 
+	}
+
+
+
+	
+	
 }
--- src/model/MemberVO.java
+++ src/model/MemberVO.java
@@ -1,5 +1,111 @@
 package model;
 
-public class MemberVO {
+import java.util.Date;
 
+public class MemberVO {
+	private String userId;
+	private String userPass;
+	private String userName;
+	private String phoneNumber;
+	private Date regDate;
+	private int selectedTime;
+	private String company;
+	private int gender;	
+	private int birth;
+	
+	public MemberVO(String userId, String userPass, String userName, String phoneNumber, Date regDate, int selectedTime,
+			String company, int gender, int birth) {
+		super();
+		this.userId = userId;
+		this.userPass = userPass;
+		this.userName = userName;
+		this.phoneNumber = phoneNumber;
+		this.regDate = regDate;
+		this.selectedTime = selectedTime;
+		this.company = company;
+		this.gender = gender;
+		this.birth = birth;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPass() {
+		return userPass;
+	}
+
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getSelectedTime() {
+		return selectedTime;
+	}
+
+	public void setSelectedTime(int selectedTime) {
+		this.selectedTime = selectedTime;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public int getBirth() {
+		return birth;
+	}
+
+	public void setBirth(int birth) {
+		this.birth = birth;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberVO [userId=" + userId + ", userPass=" + userPass + ", userName=" + userName + ", phoneNumber="
+				+ phoneNumber + ", regDate=" + regDate + ", selectedTime=" + selectedTime + ", company=" + company
+				+ ", gender=" + gender + ", birth=" + birth + "]";
+	}
+	
+	
+	
 }
